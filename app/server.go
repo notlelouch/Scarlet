8a587a7ed920223d8a5664cc527251c12b0510d9
notlelouch <notlelouchvb@gmail.com>
2024-08-18 17:24:20 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,23 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	defer l.Close()
+
+	for {
+		// Accept an incoming connection
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		// Handle the connection
+		// go handleConnection(conn)    ####### for handling multiple connections simultaneously
+		handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	input, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error reading from connection", err)
+		return
+	}
+	fmt.Printf("input is: %s", input)
+	command_lenght, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	fmt.Printf("input is: %s", command_lenght)
+
+	command, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	fmt.Printf("input is: %s", command)
+	if strings.TrimSpace(command) == "PING" {
+		// fmt.Fprint(conn, "+PONG\r\n")
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
